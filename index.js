55e6c081301e3d32ccf4f95e4c439cf45bcede8e
Yakov Rakhamimov <jacobra19@gmail.com>
2021-09-26 22:23:21 +0300

    getOptions and removed dead code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 import cliMd from 'cli-markdown';
+import fs from 'fs';
 
 const README_URL =
     'https://raw.githubusercontent.com/sudheerj/javascript-interview-questions/master/README.md';
@@ -8,7 +9,23 @@ const run = async () => {
     const readme = await getFile(README_URL);
     const exercisesStrs = getExercisesStrs(readme);
     const exercises = exercisesStrs.map(parseExercise).filter(Boolean);
-    console.log(`exercises.length`, exercises.length)
+    const json = JSON.stringify(exercises, null, 4);
+    console.log(`exercises.length`, exercises.length);
+    fs.writeFile('output.json', json, 'utf8', (err) => {
+        if (err) {
+            console.log(`error`, err);
+        }
+        fs.readFile('output.json', 'utf8', (err, data) => {
+            if (err) {
+                console.log(`error`, err);
+            }
+            const exercises = JSON.parse(data);
+            console.log(`exercises length`, exercises.length);
+            console.log(cliMd(exercises[50].fullQuestion));
+            exercises[50].options.map((option) => console.log(cliMd(option)));
+            console.log(`exercises[50]`, cliMd(exercises[50].answer.full));
+        });
+    });
 };
 
 const getFile = async (url) => {
@@ -25,55 +42,49 @@ const getExercisesStrs = (readme) => {
     return exercises;
 };
 
-const parseExercise = (exerciseStr,index) => {
+const getOptions = (exerciseStr) => {
+    const options = [];
+    let iterator = 1;
+    let shouldContinue = true;
+    while (shouldContinue) {
+        let option = exerciseStr
+            ?.split(`- ${iterator}: `)?.[1]
+            ?.split('\n')?.[0];
+        console.log(`option`, option);
+        if (!option) {
+            shouldContinue = false;
+        } else {
+            options.push(option);
+            iterator++;
+        }
+    }
+    return options;
+};
+
+const parseExercise = (exerciseStr, index) => {
     try {
         const question = exerciseStr.split('####')[1].split('\n')[0];
+        const fullQuestion = exerciseStr.split('####')[1].split('- 1:')[0];
         const answer = {
-            index: parseInt(exerciseStr.split('##### Answer:')[1].split('\n')[0]),
+            index: parseInt(
+                exerciseStr.split('##### Answer:')[1].split('\n')[0]
+            ),
             full: exerciseStr.split('</summary>')[1].split('</details>')[0],
         };
         // TODO: add logic to parse options with defferent options sizes
-        const options = [
-            exerciseStr.split('- 1: ')[1].split('\n')[0],
-            exerciseStr.split('- 2: ')[1].split('\n')[0],
-            exerciseStr.split('- 3: ')[1].split('\n')[0],
-            exerciseStr.split('- 4: ')[1].split('\n')[0],
-        ];
-    
+
+        const options = getOptions(exerciseStr);
+
         return {
             question,
+            fullQuestion,
             answer,
             options,
         };
     } catch (error) {
-        console.log(index,`error.message`, error.message)
-        return null
+        console.log(index, `error.message`, error.message);
+        return null;
     }
-
 };
 
 run();
-
-// fetch(
-//     `https://raw.githubusercontent.com/sudheerj/javascript-interview-questions/master/README.md`
-// )
-//     .then((res) => {
-//         // console.log(`res`, res)
-//         return res.text();
-//     })
-//     .then((text) => {
-//         // console.log(`text`, text);
-
-//         const parsed = [exercises[54]].map(parseExercise);
-//         console.log(`parsed`, parsed);
-//         console.log(`exercises.length`, exercises.length);
-//         // console.log(`exercises[55]`, exercises[55])
-//         // console.log(cliMd(exercises[14]));
-//     })
-
-//     .then((article) => {
-//         console.log(`article`, article);
-//     })
-//     .catch((err) => {
-//         console.log(`err`, err);
-//     });
